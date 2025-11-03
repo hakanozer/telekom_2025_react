9412b9571347f2d52fbe9484cc59799e92e8296c
Hakan ÖZER <hakanozer02@gmail.com>
2025-11-03 15:50:56 +0300

    Days_1

---
+++ src/pages/Login.tsx
@@ -0,0 +1,38 @@
+import React, { FormEvent, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const sendLogin = (evt: FormEvent) => {
+    evt.preventDefault()
+    console.log(email, password)
+  }
+
+  return (
+    <>
+      <div className="row">
+        <div className='col-sm-4'></div>
+        <div className='col-sm-4'>
+          <h2>User Login</h2>
+          <form onSubmit={sendLogin}>
+            <div className='mb-3'>
+              <input onChange={(evt) => setEmail(evt.target.value)} type='email' className='form-control' placeholder='E-Mail' />
+            </div>
+            <div className='mb-3'>
+              <input onChange={(evt) => setPassword(evt.target.value)}  type='password' className='form-control' placeholder='Password' />
+            </div>
+            <div className='d-flex justify-content-between'>
+              <button className='btn btn-success'>Login</button>
+              <NavLink to={'/register'} className='btn btn-danger'>Register</NavLink>
+            </div>
+          </form>
+        </div>
+        <div className='col-sm-4'></div>
+      </div>
+    </>
+  )
+}
+
+export default Login
