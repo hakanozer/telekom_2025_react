b1002d7b1e88cc63d2288c7707275255ca4a168d
Hakan ÖZER <hakanozer02@gmail.com>
2025-11-07 11:53:26 +0300

    Days_5

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import { allUser } from '../services/usersService'
+import { SingleUser } from '../models/iAllUser'
 
 function Users() {
   const [count, setCount] = useState(0)
@@ -7,15 +9,22 @@ function Users() {
   const call = (num: number) => {
     console.log("⚙️ Ağır işlem çalıştı")
     let total = 0
-    for (let i = 0; i < 1_000_000_000; i++) { // Simülasyon
+    for (let i = 0; i < 1_000; i++) { // Simülasyon
       total += i
     }
     return num * 10 + total
   }
-
   const result = useMemo(() => call(count), [count])
-
   console.log("🌀 Component render oldu")
+
+  const [users, setUsers] = useState<SingleUser[]>([])
+  useEffect(() => {
+    allUser().then(res => {
+      const dt = res.data
+      setUsers(dt.data)
+    })
+  }, [])
+  
 
   return (
     <>
@@ -27,6 +36,35 @@ function Users() {
       </button>
       <p>Sonuç: {result}</p>
       <p>Other: {other}</p>
+      <hr />
+      <h2>Users</h2>
+
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Profile</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {users.map((item, index) => 
+            <tr role='button' key={index}>
+              <th scope="row">{item.id}</th>
+              <td>
+                <img style={{width: 88,}} src={item.profile} className='img-thumbnail rounded-circle' />
+              </td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+            </tr>
+          )}
+          
+        </tbody>
+      </table>
     </>
   )
 }
