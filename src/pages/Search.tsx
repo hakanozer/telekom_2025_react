5611efa71a41d9db5a5f2497cef1a1e1e137ffc2
Hakan ÖZER <hakanozer02@gmail.com>
2025-11-07 15:19:56 +0300

    Search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { searchProduct } from '../services/productService'
+import { iProduct } from '../models/iAllProduct'
+import ProductItem from '../components/ProductItem'
 
 function Search() {
 
+  const [proArr, setproArr] = useState<iProduct[]>([])
   const [params, setParams] = useSearchParams()
   useEffect(() => {
     const q = params.get('q')
     if (q) {
-        console.log(q)
+        searchProduct(q).then(res => {
+            const dt = res.data
+            setproArr(dt.data)
+        })
     }
   }, [])
   
 
   return (
-    <div>Search</div>
+    <>
+      <h2>Search</h2>
+      <div className='row'>
+        {proArr.map((item, index) =>
+          <div className='col-xs-12 col-sm-6 col-md-4 col-lg-3' key={index}>
+            <ProductItem item={item}/>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -14,6 +14,15 @@ export const singleProduct = (id: string) => {
     return apiConfig.get<iSingleProduct>('products/'+id)
 }
 
+export const searchProduct = (q:string) => {
+    const sendObj = {
+        query: q,
+        page: 1,
+        per_page: 10
+    }
+    return apiConfig.get<iAllProduct>('products/search', {params: sendObj})
+}
+
 // Body -> apiConfig.post<iUser>('auth/login', sendObj)
 // QueryString -> apiConfig.get('products', {params: sendObj})
 // Path Variable -> apiConfig.get('products/'+id)
